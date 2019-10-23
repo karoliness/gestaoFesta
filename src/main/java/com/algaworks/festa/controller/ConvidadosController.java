737c3895495ac9b7298cc4325c01a5a6fa939b7f
karoline <karolinessilva@live.com>
2019-10-23 18:10:57 -0300

    finalizacao do dia 23

--- src/main/java/com/algaworks/festa/controller/ConvidadosController.java
+++ src/main/java/com/algaworks/festa/controller/ConvidadosController.java
@@ -1,22 +1,34 @@
 package com.algaworks.festa.controller;
 
+import com.algaworks.festa.model.Convidado;
 import com.algaworks.festa.repository.Convidados;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/convidados")
 public class ConvidadosController {
 
     @Autowired
     private Convidados convidados;
 
-    @GetMapping("/convidados")
-    public ModelAndView listar(){
+    @GetMapping
+    public ModelAndView listar() {
         ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+        mv.addObject(new Convidado());
         modelAndView.addObject("convidados", convidados.findAll());
+
         return modelAndView;
+    }
+
+    @PostMapping
+    public String salvar(Convidado convidado) {
+        this.convidados.save(convidado);
+        return "redirect:/convidados";
     }
 }
